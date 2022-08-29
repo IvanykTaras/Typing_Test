748630a6b16dd1bed9adc3b6bf798d2ae3d04c4f
TarasIvanyk <hanekava4@gmail.com>
2022-08-29 11:40:40 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Typing_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(new String(inputWord(new List<char>()).ToArray()));
+        }
+
+        static List<char> inputWord(List<char> c)
+        {
+            char letter = Console.ReadKey().KeyChar;
+            if (letter != ' ' && char.IsLetter(letter))
+            {
+                c.Add(letter);
+                return inputWord(c);
+            }
+            else {
+                return c;
+            }
+            
+        }
+    }
+}
