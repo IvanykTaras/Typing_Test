64f86b94206df52b1ef52e4bf0cd3414b33977dc
TarasIvanyk <hanekava4@gmail.com>
2022-08-29 13:12:13 +0200

    create LIst for words

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Typing_Test
 {
     internal class Program
     {
+        static List<(string word, bool correct)> words = new List<(string word, bool correct)> ();
+        
+
         static void Main(string[] args)
         {
+            for (int i = 0; i < 4; i++)
+            {
+                words.Add(("word",true));
+            } 
 
-            Console.WriteLine(new String(inputWord(new List<char>()).ToArray()));
+           
+            
+            //Console.WriteLine(new String(inputWord(new List<char>()).ToArray()));
         }
 
         static List<char> inputWord(List<char> c)
@@ -22,6 +32,13 @@ namespace Typing_Test
                 return c;
             }
             
+        }
+
+
+        static void write(string text, ConsoleColor color = ConsoleColor.White )
+        {   Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
